04f4cdb0bd30c5dcb81c0ebe6c395c0ab515abf5
Aditya <125716378+adiattri@users.noreply.github.com>
2025-12-15 14:41:25 +0530

    array practice 2
    kadanes algorithm, binary search

---
+++ Array/binarySearch.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class binarySearch {
+    public static int BinarySearch(int numbers[], int key) {
+        int start = 0;
+        int end = numbers.length - 1;
+        
+
+        while(start<=end){
+
+            int mid = (start + end)/2;
+            if(numbers[mid] == key){
+                return mid;
+            }
+            else if (numbers[mid] < key){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        //pre array should be sorted
+
+        int numbers[] = {2,4,6,8,10,12,14};
+        int key =  10;
+
+        System.out.println("index for key is " + BinarySearch(numbers, key));
+    }
+}
