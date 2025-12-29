a6dfcb52668e1e647db85ffac9a5d02485fd7d78
Aditya <125716378+adiattri@users.noreply.github.com>
2025-12-29 14:05:05 -0500

    backtracking practice

---
+++ Backtracking/subsets.java
@@ -0,0 +1,19 @@
+public class subsets {
+    public static void findSubsets(String str, String ans, int i){
+        if(i == str.length()){
+            System.out.println(ans);
+            return;
+        }
+
+        //recursion
+        //yes choice
+        findSubsets(str, ans+str.charAt(i), i+1);
+        findSubsets(str, ans, i+1);
+
+    } 
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubsets(str, "", 0);
+        
+    }
+}
