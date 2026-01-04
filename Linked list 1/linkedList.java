8e51f6c86da7a75909cbec31d795b8865557d675
Aditya <125716378+adiattri@users.noreply.github.com>
2026-01-04 16:12:02 -0500

    merge sort ll

--- Linked list 1/linkedList.java
+++ Linked list 1/linkedList.java
@@ -213,25 +213,87 @@ public boolean iscycle(){
     }
     return false;
 }
+private Node getMid(Node head){
+    Node slow = head;
+    Node fast = head.next;
+
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+}
+
+public Node merge(Node head1, Node head2){
+    Node mergedLL = new Node(-1);
+    Node temp = mergedLL;
+
+    while(head1 != null && head2 != null){
+        if(head1.data <= head2.data){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        else{
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+    }
+    while(head1 != null){
+        temp.next = head1;
+        head1 = head1.next;
+        temp = temp.next;
+    }
+    while(head2 != null){
+        temp.next = head2;
+        head2 = head2.next;
+        temp = temp.next;
+    }
+    return mergedLL.next;
+}
+
+public Node mergeSort(Node head){
+    if(head == null || head.next == null){
+        return head;
+    }
+    Node mid = getMid(head);
+    Node rightHead = mid.next;
+    mid.next = null;
+    Node newLeft = mergeSort(head);
+    Node newRight = mergeSort(rightHead);
+    return merge(newLeft, newRight);
+}
 
     public static void main(String[] args) {
         linkedList ll = new linkedList();
-        ll.addFirst(2);
+        // ll.addFirst(2);
+        // ll.addFirst(1);
+        // ll.addLast(3);
+        // ll.addLast(4);
+        // ll.add(2, 9);
+        // ll.print();
+        // // System.out.println(ll.size);
+        // // ll.removeFirst();
+        // // ll.print();
+        // // ll.removeLast();
+        // // ll.print();
+        // // System.out.println(ll.itersearch(2));
+        // // ll.reverse();
+        // // ll.print();
+        // ll.deleteNthFromEnd(3);
+        // ll.print();
         ll.addFirst(1);
-        ll.addLast(3);
-        ll.addLast(4);
-        ll.add(2, 9);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+
         ll.print();
-        // System.out.println(ll.size);
-        // ll.removeFirst();
-        // ll.print();
-        // ll.removeLast();
-        // ll.print();
-        // System.out.println(ll.itersearch(2));
-        // ll.reverse();
-        // ll.print();
-        ll.deleteNthFromEnd(3);
+        ll.head = ll.mergeSort(ll.head);
         ll.print();
+
+
 
     }
 }
