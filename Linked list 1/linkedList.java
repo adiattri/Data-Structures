7f2371f5da2d425711981e8baf8202e97c81e417
Aditya <125716378+adiattri@users.noreply.github.com>
2026-01-04 02:22:19 -0500

    detect cycle in linked list

--- Linked list 1/linkedList.java
+++ Linked list 1/linkedList.java
@@ -201,6 +201,19 @@ public class linkedList{
     return true;
 }
 
+public boolean iscycle(){
+    Node slow = head;
+    Node fast = head;
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow == fast){
+            return true;
+        }   
+    }
+    return false;
+}
+
     public static void main(String[] args) {
         linkedList ll = new linkedList();
         ll.addFirst(2);
